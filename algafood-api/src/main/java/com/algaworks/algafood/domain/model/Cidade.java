0a21c6b89c7030b226f8772fa06d9ecf6b774a95
Clever Junior <cleveranalista@gmail.com>
2023-01-05 00:03:17 -0300

    Corrigindo conflitos no projeto

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cidade.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cidade.java
@@ -8,8 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
